aecf7c8889375ac9174924cd0dd2808a5a39e95d
Arthur Reis <arthurreis074@gmail.com>
2023-03-26 00:19:44 -0300

    refactor: refactor jest e2e config file

--- jest-e2e.config.js
+++ jest-e2e.config.js
@@ -1,10 +1,9 @@
-const { pathsToModuleNameMapper } = require("ts-jest/utils")
-const { compilerOptions } = require("./tsconfig.build.json")
-
 module.exports = {
 	preset: "jest-playwright-preset",
 	testMatch: ["<rootDir>/tests/e2e/**/*.e2e.{ts,js}"],
-	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" }),
+	moduleNameMapper: {
+		"^@/(.*)$": "<rootDir>/src/$1",
+	},
 	transform: {
 		"^.+\\.(t|j)sx?$": ["@swc/jest"]
 	},
